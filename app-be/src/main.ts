e34c5364a47178f5873842537b9160157cda6d27
Mykhailo Kapinos <mikhail.kapinos@gmail.com>
2024-09-26 20:14:02 +0300

    Fixed server

--- app-be/src/main.ts
+++ app-be/src/main.ts
@@ -1,8 +1,16 @@
 import express, { Response, Request } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
 
 const server = express();
-const port = 3000;
 
 server.get("/", (req: Request, res: Response) => res.send("Hello world"));
 
-server.listen(port);
+server.listen(process.env.PORT, () =>
+  console.log(`Server start on http://localhost:${process.env.PORT}`)
+);
